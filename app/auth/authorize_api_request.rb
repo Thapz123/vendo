9ab7d9c6ebe677d5c38b111b5e33e13eea949e57
DouglasDDo <dougddo@gmail.com>
2017-06-06 20:47:31 -0700

    Creates auth service to check for and decode jwt's and return a User object

---
+++ app/auth/authorize_api_request.rb
@@ -0,0 +1,41 @@
+class AuthorizeApiRequest
+  def initialize(headers = {})
+    @headers = headers
+  end
+
+  # Returns user object
+  def call
+    {
+      user: user
+    }
+  end
+
+  private
+
+  attr_reader :headers
+
+  def user
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+
+  rescue ActiveRecord::RecordNotFound => e
+
+    raise(
+      ExceptionHandler::InvalidToken,
+      ("#{Message.invalid_token} #{e.message}")
+    )
+  end
+
+  # Decode authentication token
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+  end
+
+  # Checks for token in 'Authorization' header
+  def http_auth_header
+    if headers['Authorization'].present?
+      return header['Authorization'].split(' ').last
+    end
+
+    raise(ExceptionHandler::MissingToken, Message.missing_token)
+  end
+end
