461e9c9a1535bc63dea94b929b83f7a7a948ff8c
DouglasDDo <dougddo@gmail.com>
2017-06-08 08:30:52 -0700

    Completes base auth controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
+
+  # Applies to all actions unless otherwise directed
+  before_action :authorize_request
+  attr_reader :current_user
+
+  private
+
+  # Validate request with token and return user if successful 
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
