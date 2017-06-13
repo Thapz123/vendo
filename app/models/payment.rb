551705321bd79f74f36938dabde20c0d7ce4affc
DouglasDDo <dougddo@gmail.com>
2017-06-13 10:20:15 -0700

    Corrects misspelling

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ApplicationRecord
   validates_presence_of :maker_id, :recipient_id, :memo, :amount
   validates_inclusion_of :visibility, in: %w(public friends private)
-  validates_numericality_of :charge, greater_than: 0.00
+  validates_numericality_of :amount, greater_than: 0.00
 
-  
+
 end
