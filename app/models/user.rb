040893b5b3cfbd6f48106eac4088f802b0774514
DouglasDDo <dougddo@gmail.com>
2017-06-13 11:14:27 -0700

    Adds charge and payment relationships to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,33 @@ class User < ApplicationRecord
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
   validates_length_of :password, in: 6 .. 128, allow_nil: false, allow_blank: false
 
-  # has_many :transactions
+  has_many(
+    :payments_made,
+    class_name: "Payment",
+    primary_key: :id,
+    foreign_key: :maker_id
+  )
+
+  has_many(
+    :payments_received,
+    class_name: "Payment",
+    primary_key: :id,
+    foreign_key: :recipient_id
+  )
+
+  has_many(
+    :charges_made,
+    class_name: "Charge",
+    primary_key: :id,
+    foreign_key: :maker_id
+  )
+
+  has_many(
+    :charges_received,
+    class_name: "Charge",
+    primary_key: :id,
+    foreign_key: :recipient_id
+  )
   # has_many :friendships
   # likes
   # comments
