3940406475aac445cdd42846e04fed0376e2f405
DouglasDDo <dougddo@gmail.com>
2017-06-13 12:20:23 -0700

    Adds relationships on charge and payment models

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -4,5 +4,18 @@ class Charge < ApplicationRecord
   validates_inclusion_of :status, in: %w(pending resolved rejected)
   validates_numericality_of :amount, greater_than: 0.00
 
+  belongs_to(
+    :maker,
+    class_name: "User",
+    primary_key: :id,
+    foreign_key: :maker_id
+  )
+
+  belongs_to(
+    :recipient,
+    class_name: "User",
+    primary_key: :id,
+    foreign_key: :recipient_id
+  )
 
 end
