0c4bfa8228f45daa71610c47137c6f23ae0e28aa
DouglasDDo <dougddo@gmail.com>
2017-06-13 13:21:11 -0700

    Corrects usage of precision and scale

--- db/migrate/20170613163039_create_payments_table.rb
+++ db/migrate/20170613163039_create_payments_table.rb
@@ -3,7 +3,7 @@ class CreatePaymentsTable < ActiveRecord::Migration[5.0]
     create_table :payments_tables do |t|
       t.integer :maker_id, null: false
       t.integer :recipient_id, null: false
-      t.decimal :amount, scale: 12, precision: 2
+      t.decimal :amount, precision: 12, scale: 2 
       t.text    :memo, null: false, limit: 200
       t.string  :visibility, null: false, default: 'public'
 
