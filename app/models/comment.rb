4a63846bfb7edf947b7f8e20141d9c1e933e3ad9
DouglasDDo <dougddo@gmail.com>
2017-06-20 19:32:39 -0700

    Updates validations and relationships

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,19 @@
 class Comment < ApplicationRecord
+  validates_presence_of :user_id, :transaction_id, :body
+  validates_length_of :body, minimum: 1, too_short: "Comments cannot be empty.", too_long: "Comments must be 200 characters or fewer."
 
   belongs_to(
-    :maker,
+    :commenting_user,
     class_name: "User",
     primary_key: :id,
     foreign_key: :user_id
   )
 
-  # belongs_to(
-  #
-  # )
+  belongs_to(
+    :commented_transaction,
+    class_name: "Transaction",
+    primary_key: :id,
+    foreign_key: :transaction_id
+  )
 
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,14 +1,18 @@
 class Like < ApplicationRecord
+  validates_presence_of :user_id, :transaction_id
 
-    belongs_to(
-      :maker,
-      class_name: "User",
-      primary_key: :id,
-      foreign_key: :user_id
-    )
+  belongs_to(
+    :liking_user,
+    class_name: "User",
+    primary_key: :id,
+    foreign_key: :user_id
+  )
 
-    # belongs_to(
-    #
-    # )
+  belongs_to(
+    :liked_transaction,
+    class_name: "Transaction",
+    primary_key: :id,
+    foreign_key: :transaction_id
+  )
 
-  end
+end
