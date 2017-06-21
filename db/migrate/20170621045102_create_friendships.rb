e6c10ef4a1050375b5bcb9a73ce1d3daf186ca81
DouglasDDo <dougddo@gmail.com>
2017-06-20 21:56:04 -0700

    Creates friendships table

---
+++ db/migrate/20170621045102_create_friendships.rb
@@ -0,0 +1,15 @@
+class CreateFriendships < ActiveRecord::Migration[5.0]
+  def change
+    create_table :friendships do |t|
+      t.integer :requester_id, null: false
+      t.integer :responder_id, null: false
+      t.string  :status, null: false, default: 'pending'
+
+      t.timestamps
+    end
+
+    add_index :requester_id
+    add_index :responder_id
+    add_index :friendships, [:requester_id, :responder_id], unique: true
+  end
+end
