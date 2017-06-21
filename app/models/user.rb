11fbdafc1389c22ab0c00ed3d7e38de0a6be52e1
DouglasDDo <dougddo@gmail.com>
2017-06-20 21:28:12 -0700

    Alters user model to use fname and lname

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates_presence_of :name, :email, :password_digest
+  validates_presence_of :first_name, :last_name, :email, :password_digest
   validates_uniqueness_of :email
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
   validates_length_of :password, in: 6 .. 128, allow_nil: false, allow_blank: false
@@ -46,5 +46,5 @@ class User < ApplicationRecord
 
   has_many :comments
 
-  
+
 end
