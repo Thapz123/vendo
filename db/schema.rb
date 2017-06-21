980697d9bbd6774efb31fc2039d24a98eaa44359
DouglasDDo <dougddo@gmail.com>
2017-06-20 22:01:26 -0700

    Migrates latest changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613163301) do
+ActiveRecord::Schema.define(version: 20170621045102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer "owner_id",                          null: false
+    t.decimal "balance",  precision: 12, scale: 2, null: false
+    t.index ["owner_id"], name: "index_accounts_on_owner_id", unique: true, using: :btree
+  end
 
   create_table "charges_tables", force: :cascade do |t|
     t.integer  "maker_id",                                                  null: false
@@ -26,6 +32,17 @@ ActiveRecord::Schema.define(version: 20170613163301) do
     t.datetime "updated_at",                                                null: false
   end
 
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "requester_id",                     null: false
+    t.integer  "responder_id",                     null: false
+    t.string   "status",       default: "pending", null: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.index ["requester_id", "responder_id"], name: "index_friendships_on_requester_id_and_responder_id", unique: true, using: :btree
+    t.index ["requester_id"], name: "index_friendships_on_requester_id", using: :btree
+    t.index ["responder_id"], name: "index_friendships_on_responder_id", using: :btree
+  end
+
   create_table "payments_tables", force: :cascade do |t|
     t.integer  "maker_id",                                                 null: false
     t.integer  "recipient_id",                                             null: false
@@ -36,12 +53,29 @@ ActiveRecord::Schema.define(version: 20170613163301) do
     t.datetime "updated_at",                                               null: false
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.integer  "maker_id",                                                  null: false
+    t.integer  "recipient_id",                                              null: false
+    t.decimal  "amount",       precision: 12, scale: 2,                     null: false
+    t.text     "memo",                                                      null: false
+    t.string   "status",                                default: "pending", null: false
+    t.string   "visibility",                            default: "public",  null: false
+    t.datetime "created_at",                                                null: false
+    t.datetime "updated_at",                                                null: false
+    t.index ["maker_id"], name: "index_transactions_on_maker_id", using: :btree
+    t.index ["recipient_id"], name: "index_transactions_on_recipient_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
+    t.string   "email",           null: false
+    t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["first_name"], name: "index_users_on_first_name", using: :btree
+    t.index ["last_name"], name: "index_users_on_last_name", using: :btree
   end
 
 end
