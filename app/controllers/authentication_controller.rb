ea6bf1882f26ab7503dbaa8135b7c515face2cf3
DouglasDDo <dougddo@gmail.com>
2017-06-21 11:09:44 -0700

    Changes what gets passed to auth service

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,8 +2,12 @@ class AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
 
   def authenticate
-    auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    auth_token = AuthenticateUser.new(auth_params).call
     json_response(auth_token: auth_token)
+  end
+
+  def signout
+    
   end
 
   private
