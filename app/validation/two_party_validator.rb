9f87a284c04c899e03ef2df764095bf5d23db559
DouglasDDo <dougddo@gmail.com>
2017-06-30 08:38:57 -0700

    Leaves note for usage in validator

--- app/validation/two_party_validator.rb
+++ app/validation/two_party_validator.rb
@@ -1,3 +1,5 @@
+# Usage: in model definitions, use validates_with TwoPartyValidator, fields: [:fieldName1, :fieldName2]
+
 class TwoPartyValidator < ActiveModel::Validator
   def validate(record)
     record.errors[:base] << "This action requires two separate User objects." if same_user
