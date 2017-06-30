7deca0dc608bc6ead397ce295835bcbfaaa631f4
DouglasDDo <dougddo@gmail.com>
2017-06-30 08:38:02 -0700

    Sets up transactions controller

---
+++ app/controllers/transactions_controller.rb
@@ -0,0 +1,26 @@
+class TransactionsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def pay
+    json_response({msg: 'hello'})
+  end
+
+  def charge
+    json_response({msg: 'byebye'})
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+end
