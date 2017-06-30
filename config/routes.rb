e5b5df2a0488cf99264b12e5cd4666c7e8be8021
DouglasDDo <dougddo@gmail.com>
2017-06-30 08:39:22 -0700

    Starts fleshing out routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,19 @@ Rails.application.routes.draw do
   # Transactions
     # Likes
     # Comments
+  resources :users, except: [:new, :create] do
+    resources :transactions, except: [:new, :create] do
+      resources :comments, only: [:create, :update, :destroy]
+      # resources :likes, only: [:create, :update, :destroy]
+    end
+  end
 
   resources :likes
 
+
   post 'login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
+
+  post ':userId/payments', to: 'transactions#pay'
+  post ':userId/charges', to: 'transactions#charge'
 end
