66715aa82aff1d0c25e9917894949e110af9d380
DouglasDDo <dougddo@gmail.com>
2017-07-01 09:02:20 -0700

    Makes email uniqueness validation case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   has_secure_password
 
   validates_presence_of :first_name, :last_name, :email, :password_digest
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :case_sensitive => false
   validates_uniqueness_of :username, :case_sensitive => false
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
   validates_length_of :password, in: 6 .. 128, allow_nil: false, allow_blank: false
 
-  after_initialize :set_username
+  after_initialize :ensure_username
   after_create :build_account
   before_save :downcase_username
 
@@ -64,7 +64,7 @@ class User < ApplicationRecord
 
   private
 
-  def set_username
+  def ensure_username
     self.username ||= "#{self.first_name.downcase}-#{self.last_name.downcase}-#{rand(1..99)}" if self.new_record?
     true
   end
