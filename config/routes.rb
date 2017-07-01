60903f73a999e748bd7499cec53b9966aa1f8cc1
DouglasDDo <dougddo@gmail.com>
2017-06-30 23:22:22 -0700

    Changes up routes to use scope with userId

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # Users
-    # Friends
-
-  # Transactions
-    # Likes
-    # Comments
-  resources :users, except: [:new, :create] do
-    resources :transactions, except: [:new, :create] do
-      resources :comments, only: [:create, :update, :destroy]
-      # resources :likes, only: [:create, :update, :destroy]
-    end
-  end
-
-  resources :likes
-
-
-  post 'login', to: 'authentication#authenticate'
+  post 'login',  to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 
-  post ':userId/payments', to: 'transactions#pay'
-  post ':userId/charges', to: 'transactions#charge'
+  scope ':userId' do
+    resources :transactions, except: [:new, :create] do
+      resources :likes, except: [:show, :destroy]
+      # resources :comments, only: [:create, :update, :destroy]
+    end
+
+    post 'pay',    to: 'transactions#pay'
+    post 'charge', to: 'transactions#charge'
+  end
+
 end
