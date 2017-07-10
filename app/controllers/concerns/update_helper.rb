a9a1c276584ccc650aa172ec5e11fc32594e1ccc
DouglasDDo <dougddo@gmail.com>
2017-07-10 06:42:28 -0700

    Creates update helper to handle updates for specific resources

--- app/controllers/concerns/update_helper.rb
+++ app/controllers/concerns/update_helper.rb
@@ -1,9 +1,17 @@
 module UpdateHelper
-  def update_with_params(object, params)
-    # go through hash
-    # if value is nil
-      # skip
-    # else
-      # update attribute
+  def update_user_with_params(user, params)
+    json_response({message: "You can only update your own account."}) unless user.username == params[:username]
+    _update_obj_with_params(user, params)
+  end
+
+  def update_transaction_with_params(object, params)
+    # TODO: needs authorization check on both sides
+  end
+
+  def _update_obj_with_params(obj, params)
+    params.each do |key, val|
+      next if val == nil
+      obj.update(key, val)
+    end
   end
 end
